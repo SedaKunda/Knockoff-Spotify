26b02060163f8524f3ce73430d47d5a12b1eb0cc
seku <sedakunda@gmail.com>
2023-12-05 15:41:44 +0000

    date conversion test init

---
+++ app/src/test/java/com/example/knockoffspotify/utils/DateUtilsTest.kt
@@ -0,0 +1,17 @@
+package com.example.knockoffspotify.utils
+
+import org.junit.Test
+
+class DateUtilsTest
+
+@Test
+fun `can get convert date`() {
+}
+
+@Test
+fun `when date is not set`() {
+}
+
+@Test
+fun `when date is set but wrong format`() {
+}
