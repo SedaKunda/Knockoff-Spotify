950d3c938f660bbdb7028fbbabf78540092a09ba
seku <sedakunda@gmail.com>
2024-09-10 14:59:01 +0100

    Use existing OffsetDateTime to parse date instead of custom function

--- app/src/main/java/com/example/knockoffspotify/utils/DateUtils.kt
+++ app/src/main/java/com/example/knockoffspotify/utils/DateUtils.kt
@@ -1,33 +1,20 @@
 package com.example.knockoffspotify.utils
 
-import android.annotation.SuppressLint
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.Locale
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 /**
- * Checks if the string is a valid date and converts it to a readable format.
+ * Converts a date string to a readable format.
  * @param conversionErrorMessage: The message to display if the conversion fails.
  * @return The date in a readable format or the conversion error message.
  */
-@SuppressLint("ConstantLocale")
-val locale: Locale = Locale.getDefault()
-
-val yearMonthFormat = SimpleDateFormat("yyyy-MM-dd", locale)
-val dayMonthFormat = SimpleDateFormat("dd.MM.yyyy", locale)
 
 fun String.toReadableDate(conversionErrorMessage: String): String {
-    return if (this.isValid()) {
-        val parsedDate = yearMonthFormat.parse(this)
-        parsedDate?.let { dayMonthFormat.format(parsedDate) } ?: conversionErrorMessage
-    } else conversionErrorMessage
-}
-
-private fun String.isValid(): Boolean {
-    return if (this != "" && !this.matches("\\d{4}-[01]\\d-[0-3]\\d.*".toRegex())) false else try {
-        yearMonthFormat.parse(this)
-        true
-    } catch (ex: ParseException) {
-        false
+    return try {
+        val offsetDateTime = OffsetDateTime.parse(this)
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        formatter.format(offsetDateTime)
+    } catch (ex: Exception) {
+        conversionErrorMessage
     }
 }
